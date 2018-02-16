f07800c5576aac492229738892c1ac0531d59b90
Oliver Veits <oliver.veits@gmail.com>
2018-02-16 19:18:07 +0100

    corrected some errors

--- 001_install_docker.sh
+++ 001_install_docker.sh
@@ -6,44 +6,45 @@
 
 # Check, whether OverlayFS is enabled:
 
-if lsmod | grep -v overlay; then
+if ! lsmod | grep -q overlay; then
    
    # Verify that the kernel is at least 3.10:
    uname -r | grep "3.[1-9][0-9]" || exit 1
    
    # Enable OverlayFS:
-   sudo tee /etc/modules-load.d/overlay.conf <<-'EOF'
-   overlay
-   EOF
+   sudo tee /etc/modules-load.d/overlay.conf <<-'EOFEnableOverlayFS'
+overlay
+EOFEnableOverlayFS
    
    echo "Rebooting to enable OverlayFS. Please re-run $0 after boot"
    read -p "Enter y to reboot" a
    [ "$a" == "y" ] && reboot
 fi
 
+
 # you should not reach here without overlay being enabled:
-lsmod | grep -v overlay && exit 1
+lsmod | grep overlay || exit 1
 
 # Configure yum to use the Docker yum repo:
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOFDockerYUMRepo'
 [dockerrepo]
 name=Docker Repository
 baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
 enabled=1
 gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
-EOF
+EOFDockerYUMRepo
 
 # Configure systemd to run the Docker Daemon with OverlayFS:
 
-sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOF
+sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<- EOFRunDockerSystemd
 [Service]
 ExecStart=
 ExecStart=/usr/bin/docker daemon --storage-driver=overlay -H fd://
-EOF
+EOFRunDockerSystemd
 
 sudo yum install --assumeyes --tolerant docker-engine-1.11.2-1.el7.centos
 sudo systemctl start docker
 sudo systemctl enable docker
 
-sudo docker ps | echo docker installation was successful!
+sudo docker ps 1>/dev/null && echo 'docker installation was successful!'
